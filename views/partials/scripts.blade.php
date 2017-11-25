ab41c2fb5c26795f14f5fafeeb12d9f978c94180
sagsoz06 <sagsoz06@gmail.com>
2017-11-25 19:44:21 +0300

    Updating Theme

--- views/partials/scripts.blade.php
+++ views/partials/scripts.blade.php
@@ -23,6 +23,25 @@
 <!-- JS Page Level -->
 {!! Theme::script('js/theme.js') !!}
 
+<script type="text/javascript"> WebFontConfig = {
+        google: {
+            families: [
+                'Roboto:400,700,300:latin-ext',
+                'Raleway:700,800,900,400,300:latin-ext',
+                'Open+Sans:400,300,600,700,800'
+            ]
+        }
+    };
+    (function () {
+        var wf = document.createElement('script');
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+    })();
+</script>
+
 @stack('js_inline')
 
 <?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false): ?>
